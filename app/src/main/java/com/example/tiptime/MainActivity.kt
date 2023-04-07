b937900194146e6731e81785d0dd1b3cc82aeee0
TNikolau <tkachuk.nikolau@chnu.edu.ua>
2023-04-07 18:50:22 +0300

    2.2_11

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.example.tiptime
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.calculateButton.setOnClickListener { calculate() }
+    }
+
+    private fun calculate() {
+        val stringInTextField = binding.IHaveAHryvnia.text.toString()
+        val cost = stringInTextField.toDoubleOrNull()
+        if (cost == null) {
+            binding.result.text = ""
+            return
+        }
+
+        val costPercentage = when (binding.tipOptions.checkedRadioButtonId) {
+            R.id.option_zero_two_four_four -> 0.0244
+            else -> 0.025
+        }
+
+        var result = costPercentage * cost
+
+        val formattedResult = NumberFormat.getCurrencyInstance().format(result)
+        binding.result.text = getString(R.string.result, formattedResult)
     }
 }
